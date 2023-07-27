46ca28830075f03ba59f6cda3075182c9069d9b7
centum-world <centumworldminingrig@gmail.com>
2023-07-27 12:33:10 +0530

    Revert "economic calender"
    This reverts commit afa0fa823d7e6130d52eca96ee2e7a9269633e69.

--- src/UserSidebarPages/EconomicCelender.js
+++ src/UserSidebarPages/EconomicCelender.js
@@ -1,84 +1,7 @@
-// import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 
-// const EconomicCelender = () => {
+const EconomicCelender = () => {
 
-//   const containerRef = useRef(null);
-//   const iframeRef = useRef(null);
-
-//   useEffect(() => {
-//     const script = document.createElement('script');
-//     script.src = 'https://s3.tradingview.com/external-embedding/embed-widget-events.js';
-//     script.async = true;
-//     script.innerHTML = JSON.stringify({
-//       colorTheme: 'light',
-//       isTransparent: false,
-//       width: '100%',
-//       height: '600',
-//       locale: 'in',
-//       importanceFilter: '-1,0,1',
-//       currencyFilter: 'USD,EUR,ITL,NZD,CHF,AUD,FRF,JPY,ZAR,TRL,CAD,DEM,MXN,ESP,GBP',
-//     });
-
-//     const container = containerRef.current;
-
-//     if (container) {
-//       container.appendChild(script);
-//     }
-
-//     const iframe = document.createElement('iframe');
-//     iframe.src = 'https://s3.tradingview.com/external-embedding/embed-widget-events.js';
-//     iframe.style.display = 'none';
-
-//     const iframeLoadHandler = () => {
-//       const iframeBody = iframe.contentDocument.body;
-//       if (iframeBody) {
-//         iframeBody.appendChild(script);
-//       }
-//       iframe.removeEventListener('load', iframeLoadHandler);
-//     };
-
-//     iframe.addEventListener('load', iframeLoadHandler);
-//     container.appendChild(iframe);
-
-//     iframeRef.current = iframe;
-
-//     return () => {
-//       iframe.removeEventListener('load', iframeLoadHandler);
-//       if (iframeRef.current && iframeRef.current.parentNode) {
-//         iframeRef.current.parentNode.removeChild(iframeRef.current);
-//       }
-//     };
-//   }, []);
-
-//   return (
-//     <>
-//       <p style={{ fontFamily: 'Calibri', fontSize: '20px', fontWeight: 600, color: '#5e72e4' }}> Economic Calender</p>
-//       <div className="tradingview-widget-container" ref={containerRef}>
-//         <div className="tradingview-widget-container__widget">
-//           <iframe
-//             title="Economic Calendar"
-//             className="tradingview-widget-container__widget"
-//             ref={iframeRef}
-//           ></iframe>
-//         </div>
-//         <div className="tradingview-widget-copyright">
-//           {/* <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank">
-//         <span className="blue-text">Track all markets on TradingView</span>
-//       </a> */}
-//         </div>
-//       </div></>
-
-//   );
-// };
-
-
-// export default EconomicCelender
-
-
-
-import React, { useEffect, useRef } from 'react';
-
-const EconomicCalendar = () => {
   const containerRef = useRef(null);
   const iframeRef = useRef(null);
 
@@ -89,7 +12,7 @@ const EconomicCalendar = () => {
     script.innerHTML = JSON.stringify({
       colorTheme: 'light',
       isTransparent: false,
-      width: '90%',
+      width: '510',
       height: '500',
       locale: 'in',
       importanceFilter: '-1,0,1',
@@ -129,29 +52,24 @@ const EconomicCalendar = () => {
 
   return (
     <>
-      <p style={{ fontFamily: 'Calibri', fontSize: '20px', fontWeight: 600, color: '#5e72e4' }}>
-        Economic Calendar
-      </p>
-      <div className="tradingview-widget-container" ref={containerRef} style={{ width: '100%' }}>
-        <div
-          className="tradingview-widget-container__widget"
-          style={{ width: '100%', height: '600' }}
-        >
+      <p style={{ fontFamily: 'Calibri', fontSize: '20px', fontWeight: 600, color: '#5e72e4' }}> Economic Calender</p>
+      <div className="tradingview-widget-container" ref={containerRef}>
+        <div className="tradingview-widget-container__widget">
           <iframe
             title="Economic Calendar"
             className="tradingview-widget-container__widget"
             ref={iframeRef}
-            style={{ width: '100%', height: '600' }}
           ></iframe>
         </div>
         <div className="tradingview-widget-copyright">
           {/* <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank">
-            <span className="blue-text">Track all markets on TradingView</span>
-          </a> */}
+        <span className="blue-text">Track all markets on TradingView</span>
+      </a> */}
         </div>
-      </div>
-    </>
+      </div></>
+
   );
 };
 
-export default EconomicCalendar;
+
+export default EconomicCelender
--- src/UserSidebarPages/TraditionalCurrencyChart.js
+++ src/UserSidebarPages/TraditionalCurrencyChart.js
@@ -1,37 +1,35 @@
-import React, { useEffect } from 'react';
+import React,{useEffect} from 'react'
 
 const TraditionalCurrencyChart = () => {
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://s3.tradingview.com/tv.js';
-    script.async = true;
-    script.onload = () => {
-      new window.TradingView.widget({
-        "width": 800, // Adjust the width as needed
-        "height": 500, // Adjust the height as needed
-        "symbol": "FX_IDC:USDINR",
-        "interval": "D",
-        "timezone": "Etc/UTC",
-        "theme": "light",
-        "style": "1",
-        "locale": "in",
-        "toolbar_bg": "#f1f3f6",
-        "enable_publishing": false,
-        "allow_symbol_change": true,
-        "container_id": "tradingview_9bd29"
-      });
-    };
-    document.body.appendChild(script);
 
-    return () => {
-      // Cleanup function to remove the script when the component unmounts
-      document.body.removeChild(script);
-    };
-  }, []);
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = 'https://s3.tradingview.com/tv.js';
+        script.async = true;
+        script.onload = () => {
+          new window.TradingView.widget({
+            "autosize": true,
+            "symbol": "FX_IDC:USDINR",
+            "interval": "D",
+            "timezone": "Etc/UTC",
+            "theme": "light",
+            "style": "1",
+            "locale": "in",
+            "toolbar_bg": "#f1f3f6",
+            "enable_publishing": false,
+            "allow_symbol_change": true,
+            "container_id": "tradingview_9bd29"
+          });
+        };
+        document.body.appendChild(script);
+    
+        return () => {
+          document.body.removeChild(script);
+        };
+      }, []);
 
   return (
-    <div className="tradingview-widget-container" style={{ width: "100%", height: "500px" }}>
-      {/* Adjust the height as needed */}
+    <div className="tradingview-widget-container">
       <div id="tradingview_9bd29"></div>
       <div className="tradingview-widget-copyright">
         <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank">
@@ -39,7 +37,8 @@ const TraditionalCurrencyChart = () => {
         </a>
       </div>
     </div>
-  );
-};
 
-export default TraditionalCurrencyChart;
+  )
+}
+
+export default TraditionalCurrencyChart
